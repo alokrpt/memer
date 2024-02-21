4d3f0cbb30bc2e84de2f3ddcbe9ccb5b40ae95d4
Alok Singh <alokrpt@gmail.com>
2024-02-21 18:41:46 +0530

    Initial Commit

---
+++ memer/memerApp.swift
@@ -0,0 +1,17 @@
+//
+//  memerApp.swift
+//  memer
+//
+//  Created by Alok Singh on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct memerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
