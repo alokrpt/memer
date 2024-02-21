34adf3a8ac37f6997e647fb367c139d278ef3622
Alok Singh <alokrpt@gmail.com>
2024-02-21 20:55:55 +0530

    added assets and basic setup completed

---
+++ memer/MemeModel.swift
@@ -0,0 +1,13 @@
+//
+//  SomeModel.swift
+//  memer
+//
+//  Created by Alok Singh on 21/02/24.
+//
+
+import Foundation
+
+struct PersonModel: Hashable {
+ let name: String
+ let age: Int
+}
---
+++ memer/Viewer.swift
@@ -0,0 +1,18 @@
+//
+//  Viewer.swift
+//  memer
+//
+//  Created by Alok Singh on 21/02/24.
+//
+
+import SwiftUI
+
+struct Viewer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Viewer()
+}
